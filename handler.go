902b4f79faa88edec2f65e313e526b944a2c7e6b
Colin <colingood0@gmail.com>
2023-03-04 21:32:09 +0800

    feat(): 修改 linebot message 的處理邏輯

--- handler.go
+++ handler.go
@@ -5,6 +5,11 @@ import (
 	"github.com/line/line-bot-sdk-go/v7/linebot"
 	"log"
 	"net/http"
+	"strings"
+)
+
+const (
+	prefix = "小僕人 "
 )
 
 func (app *AppConfig) health(c *gin.Context) {
@@ -24,8 +29,15 @@ func (app *AppConfig) lineCallback(c *gin.Context) {
 		if event.Type == linebot.EventTypeMessage {
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
-				if _, err = app.Bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
-					log.Print(err)
+				if strings.HasSuffix(message.Text, prefix) {
+					input := strings.Split(message.Text, prefix)[1]
+					reply := "你說的是" + input
+					if input == "" {
+						reply = "你有說話嗎？"
+					}
+					if _, err = app.Bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(reply)).Do(); err != nil {
+						log.Print(err)
+					}
 				}
 			}
 		}
